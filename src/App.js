594212aa9f00fa89f983ceeea1333ef06a4f7963
pradiptisingh19 <pradipti.singh8@gmail.com>
2025-07-24 16:31:31 +0530

    created different pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Dashboard from "./pages/dashboard";
+import Spent from "./components/spent";
+import Lent from "./components/lent";
+import Profile from "./components/profile";
 import './App.css';
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<Spent />} />
+          <Route path="spent" element={<Spent />} />
+          <Route path="lent" element={<Lent />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Routes>
     </Router>
   );
